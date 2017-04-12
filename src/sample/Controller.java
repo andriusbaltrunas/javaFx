d7deed72a377711c5029cd18f8d295c8b4ccb5c2
andriusbaltrunas <andriusbaltrunas@yahoo.com>
2017-04-12 19:55:18 +0300

    Initial commit

---
+++ src/sample/Controller.java
@@ -0,0 +1,16 @@
+package sample;
+
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.Label;
+
+public class Controller {
+
+    @FXML
+    private Label text;
+
+    public void sayHi(ActionEvent event){
+        String prefix = text.getText();
+        text.setText(prefix + " Andrius");
+    }
+}
